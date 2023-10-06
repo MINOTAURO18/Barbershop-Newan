b3e83d36ba3a9f2613083f268679ab212c988e6e
alexander muñoz <alexander12345my@gmail.com>
2023-10-05 19:04:14 -0500

    sectionOne 90%

--- src/components/sectionOne/SectionOne.tsx
+++ src/components/sectionOne/SectionOne.tsx
@@ -1,14 +1,20 @@
 import styles from './sectionOne.module.css'
 import whatsapp from '../../assets/whatsapp.svg'
+import bigotes from '../../assets/bigotes.svg'
 
 
 const SectionOne = () => {
     return(
-        <section className={`${styles.sectionOne} flex flex-col justify-center items-center text-white`}>
+        <section className={`${styles.sectionOne} flex flex-col justify-center items-center gap-5 text-white`}>
             <span className='text-2xl font-semibold'>Newan</span>
-            <h1>BARBERSHOP</h1>
+            <h1 className='text-6xl font-semibold'>BARBERSHOP</h1>
+            <div className='flex justify-center items-center gap-3'>
+                <hr className='w-32' />
+                <img className='w-7' src={bigotes} alt="" />
+                <hr className='w-32' />
+            </div>
             <div>
-                <a href="#">
+                <a href="#" className='flex items-center justify-center border-2 border-amber-400 rounded-md gap-1 w-36 h-11'>
                     <img className='w-6' src={whatsapp} alt="logo de whatsapp" />
                     <span>Contactanos</span>
                 </a>
