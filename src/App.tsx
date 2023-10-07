1164d2873747a71f5f83af979f95f8dd7a25de2a
alexander muñoz <alexander12345my@gmail.com>
2023-10-07 11:34:00 -0500

    remove de darkMode

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import SectionOne from './components/sectionOne/SectionOne'
 import SectionTwo from './components/sectionTwo/SectionTwo'
 import SectionThree from './components/sectionThree/SectionThree'
 import SectionFour from './components/sectionFour/SectionFour'
-import { useState } from 'react'
 import './index.css'
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(false)
 
  
 
   return (
-    <main className={darkMode === false ? 'default' : 'dark'}>
-      <NavBar darkMode={darkMode} setDarkMode={setDarkMode}/>
+    <main>
+      <NavBar />
       <SectionOne/>
       <SectionTwo/>
       <SectionThree/>
--- src/components/sectionTwo/SectionTwo.tsx
+++ src/components/sectionTwo/SectionTwo.tsx
@@ -6,15 +6,15 @@ const SectionTwo = () => {
   return (
     <section className={`${styles.sectionTwo} flex flex-col gap-3 text-black px-8% py-20`}>
       
-      <h2 className="text-2xl font-semibold">Titulo</h2>
-      <div className="flex justify-center items-center gap-3 mb-5">
+      <h2 className={`${styles.efect} text-2xl font-semibold`}>Titulo</h2>
+      <div className={`${styles.efect} flex justify-center items-center gap-3 mb-5`}>
         <hr className="w-32" />
         <img className={` ${styles.bigote} w-8`} src={shair} alt="" />
         <hr className="w-32" />
       </div>
       <article className={`${styles.container} flex justify-center flex-wrap items-center gap-20`}>
-      <img className={`${styles.bg2}`} src={bg2} alt="" />
-       <div className="flex flex-col gap-4">
+      <img className={`${styles.bg2} ${styles.efect}`} src={bg2} alt="" />
+       <div className={`${styles.efect} flex flex-col gap-4`}>
           <h3 className="font-semibold text-2xl">Servicios</h3>
           <div className="flex flex-col gap-3 w-96">
             <span className="flex justify-between">
